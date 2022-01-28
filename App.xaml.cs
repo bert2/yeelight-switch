d393df52b6b06cee5e500c246fb3261f2b295cef
bert2 <shuairan@gmail.com>
2022-01-28 14:23:15 +0100

    app with dark material theme

---
+++ App.xaml.cs
@@ -0,0 +1,8 @@
+﻿namespace Yeelight.Switch
+{
+    using System.Windows;
+
+    public partial class App : Application
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Yeelight.Switch
+{
+    using MaterialDesignExtensions.Controls;
+
+    public partial class MainWindow : MaterialWindow
+    {
+        public MainWindow() => InitializeComponent();
+    }
+}
